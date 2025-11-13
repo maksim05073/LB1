2ad856c52aaeb7403cb9519b550ea584a3cabd9a
maksim05073 <maksimmixcer@gmail.com>
2025-11-13 21:45:45 +0200

    Add files via upload

---
+++ lb1_3/lb1_3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using lb1_3;
+
+record PrintJob(string User, string Doc, int Priority);
+record PrintStat(string User, string Doc, DateTime Time);
+
+class Program
+{
+    static void Main()
+    {
+        var printer = new Printer();
+        
+        printer.Add("Oleg", "Report.pdf", 5);
+        printer.Add("Boss", "Important.doc", 1);
+        printer.Add("Anna", "Photo.jpg", 10);
+        
+        printer.ProcessAll();
+        
+        printer.ShowStats();
+        printer.SaveStats("stats.txt");
+        Console.ReadKey();
+    }
+}
